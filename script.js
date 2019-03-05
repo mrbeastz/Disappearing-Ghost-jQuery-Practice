434561f62f910ad2e806e192a71ee71a42f5bdcb
mrbeastz <43611204+mrbeastz@users.noreply.github.com>
2019-03-05 09:32:47 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,71 @@
+$("#hide-button  ").click(function() {
+    $(" img ").hide();
+});
+
+
+$("#show-button  ").click(function() {
+    $(" img ").show();
+
+});
+
+
+$("#say-boo  ").click(function() {
+    $(" button ").text("boo");
+
+});
+
+
+
+$("#say-boo  ").click(function() {
+    $(" button ").text("boo");
+
+});
+
+
+
+
+$("#say-bye  ").click(function() {
+    $(" button ").text("bye");
+
+});
+
+
+
+$("#make-yellow  ").click(function() {
+    $(" body ").css("background-color","yellow "); 
+
+});
+
+
+
+$("#make-aqua  ").click(function() {
+    $(" body ").css("background-color","aqua "); 
+
+});
+
+
+
+$("#happy-ghost  ").click(function() {
+    $(" #ghost1 ").attr("src","https://gaia.adage.com/images/bin/image/rightrail/PimpedOutGhost_cr_Kelsey_Dake.png?1386197384");
+    
+    }
+);
+
+
+
+$("#cool-ghost  ").click(function() {
+    $(" #ghost1 ").attr("src","https://cdn.drawception.com/images/panels/2017/7-1/GHxKHjNOBp-6.png");
+    
+    }
+);
+
+
+ 
+
+
+
+
+
+
+
+
